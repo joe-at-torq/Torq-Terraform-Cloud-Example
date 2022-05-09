820303f4ed7fe219fd289262fb202aa80697d22d
Joe Dillig <97898347+joe-at-torq@users.noreply.github.com>
2022-05-09 11:25:15 -0500

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "client_id" {
+  type = string
+  default  = ""
+}
+
+variable "client_secret" {
+  type = string
+  default  = ""
+}
+
+variable "tenant_id {
+  type = string
+  default  = ""
+}
