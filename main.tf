08fdd6db6740b285ad6989a7eec9e7ab97dc1fc9
Joe Dillig <97898347+joe-at-torq@users.noreply.github.com>
2022-05-10 14:31:26 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ variable "client_side_region" {
 
 #Resouce Group
 resource "azurerm_resource_group" "client_rg" {
-  name     = "Torq_Terraform_Cloud"
+  name     = "Torq_Terraform_Cloud2"
   location = var.client_side_region
 }
 
